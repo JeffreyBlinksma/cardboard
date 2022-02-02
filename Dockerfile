4e80bb8324e51936d2a7ae26470b11e61b7c7481
Jeffrey Blinksma <54038477+JeffreyBlinksma@users.noreply.github.com>
2022-02-02 15:28:11 +0100

    Initial commit
    Created .dockerignore, docker-compose.yml, Dockerfile, main.py and requirements.txt.
    Dockerfile contains instructions on how to build latest image with Python and pyodbc.
    main.py has StartTransaction implemented, but it has not been tested yet. Other requests and logging are to be implemented at a later date.

---
+++ Dockerfile
@@ -0,0 +1,46 @@
+FROM python:3.10.2-slim AS base
+
+COPY requirements.txt .
+
+# Setup dependencies for pyodbc
+RUN \
+  export ACCEPT_EULA='Y' && \
+  export MYSQL_CONNECTOR='mysql-connector-odbc-8.0.28-linux-glibc2.12-x86-64bit' && \
+  export MYSQL_CONNECTOR_CHECKSUM='003a5e45830f103fa303743179e20fb6' && \
+  apt-get update && \
+  apt-get install -y curl build-essential unixodbc-dev g++ apt-transport-https && \
+  gpg --keyserver hkp://keys.gnupg.net --recv-keys 5072E1F5 && \
+  #
+  # Install pyodbc db drivers for MSSQL, PG and MySQL
+  curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add - && \
+  curl https://packages.microsoft.com/config/debian/9/prod.list > /etc/apt/sources.list.d/mssql-release.list && \
+  curl -L -o ${MYSQL_CONNECTOR}.tar.gz https://dev.mysql.com/get/Downloads/Connector-ODBC/8.0/${MYSQL_CONNECTOR}.tar.gz && \
+  curl -L -o ${MYSQL_CONNECTOR}.tar.gz.asc https://downloads.mysql.com/archives/gpg/\?file\=${MYSQL_CONNECTOR}.tar.gz\&p\=10 && \
+  gpg --verify ${MYSQL_CONNECTOR}.tar.gz.asc && \
+  echo "${MYSQL_CONNECTOR_CHECKSUM} ${MYSQL_CONNECTOR}.tar.gz" | md5sum -c - && \
+  apt-get update && \
+  gunzip ${MYSQL_CONNECTOR}.tar.gz && tar xvf ${MYSQL_CONNECTOR}.tar && \
+  cp ${MYSQL_CONNECTOR}/bin/* /usr/local/bin && cp ${MYSQL_CONNECTOR}/lib/* /usr/local/lib && \
+  myodbc-installer -a -d -n "MySQL ODBC 8.0 Driver" -t "Driver=/usr/local/lib/libmyodbc8w.so" && \
+  myodbc-installer -a -d -n "MySQL ODBC 8.0" -t "Driver=/usr/local/lib/libmyodbc8a.so" && \
+  apt-get install -y msodbcsql17 odbc-postgresql && \
+  #
+  # Update odbcinst.ini to make sure full path to driver is listed
+  sed 's/Driver=psql/Driver=\/usr\/lib\/x86_64-linux-gnu\/odbc\/psql/' /etc/odbcinst.ini > /tmp/temp.ini && \
+  mv -f /tmp/temp.ini /etc/odbcinst.ini && \
+  # Install dependencies
+  pip install --upgrade pip && \
+  pip install -r requirements.txt && rm requirements.txt && \
+  # Cleanup build dependencies
+  rm -rf ${MYSQL_CONNECTOR}* && \
+  apt-get remove -y curl apt-transport-https debconf-utils g++ gcc rsync unixodbc-dev build-essential gnupg2 && \
+  apt-get autoremove -y && apt-get autoclean -y
+
+WORKDIR /app
+
+# Add your source files.
+COPY main.py .
+# Install dependencies
+RUN pip install pyOpenSSL zeep
+
+CMD ["python", "main.py"]
---
+++ main.py
@@ -0,0 +1,106 @@
+# Import dependencies
+import time
+import pyodbc
+from OpenSSL import crypto
+import string
+import random
+from zeep import Client
+import os
+import logging.config
+
+# Set variables
+StoredID = 0
+
+# Import private key
+key_file = open("/configs/key.pem", "r")
+key = key_file.read()
+key_file.close()
+pkey = crypto.load_privatekey(crypto.FILETYPE_PEM, key)
+
+# Load SOAP Client
+zeepclient = Client("https://wecr.sepay.nl/v2/wecr.asmx?WSDL")
+
+# Debug
+if os.environ['DEBUG'] == True:
+    logging.config.dictConfig({
+        'version': 1,
+        'formatters': {
+            'verbose': {
+                'format': '%(name)s: %(message)s'
+            }
+        },
+        'handlers': {
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': 'verbose',
+            },
+        },
+        'loggers': {
+            'zeep.transports': {
+                'level': 'DEBUG',
+                'propagate': True,
+                'handlers': ['console'],
+            },
+        }
+    })
+
+server = os.environ['SQLServer']
+database = os.environ['SQLDatabase']
+username = os.environ['SQLUser']
+password = os.environ['SQLPassword']
+
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
+
+while True:
+
+    time.sleep(1)
+
+    # Grab last entry from Payment, and print the DocumentID, PaymentTypeID and Amount
+    cursor.execute("SELECT DocumentID, PaymentTypeID, Amount FROM "+database+".dbo.Payment ORDER BY DocumentID DESC")
+    row = cursor.fetchone()
+
+    if StoredID < row[0]:
+        StoredID = row[0]
+
+        if row[1] == os.environ['PaymentID']:
+            
+            while True:
+            
+                    # Convert Amount into string with 2 decimals, as required by the API
+                ConvertedAmount = "{:.2f}".format(row[2])
+
+                # Generate TransactionRef
+                TransactionRef = ''.join(random.choices(string.ascii_uppercase + string.digits, k=9))
+
+                # Create Signature
+                SignatureData = f"0;2;{os.environ['SepayLogin']};{str(os.environ['SID'])};{TransactionRef};{str(row[0])};{ConvertedAmount};"
+                SignatureSign = crypto.sign(pkey, SignatureData, "sha256")
+
+                RequestResult = Client.service.StartTransaction(key_index=0, version="2", login=os.environ['SepayLogin'], sid=os.environ['SID'], transactionref=TransactionRef, merchantref=str(row[0]), amount=ConvertedAmount, signature=SignatureSign)
+
+                # Check status code and retry if neccesary 
+                match RequestResult["status"]:
+                    case 0:
+                        break
+                    case 1:
+                        print("Some of the required fields are missing: "+ RequestResult['message'])
+                        break
+                    case 2:
+                        print("Signature failed")
+                        break
+                    case 4:
+                        continue
+                    case 6:
+                        print("Duplicate request")
+                        break
+                    case 7:
+                        print("Terminal not active or not enabled and/or authorized for transactions through WECR")
+                        break
+                    case 11:
+                        print("Pending request for this terminal.")
+                        break
+                    case 99:
+                        print("Undefined error")
+                        break
