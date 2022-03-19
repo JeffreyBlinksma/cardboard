d634bd94241940068586d0f7c3c06f3f7df1ffcf
Jeffrey Blinksma <54038477+JeffreyBlinksma@users.noreply.github.com>
2022-03-19 11:04:55 +0100

    Fixed bug where transaction is sent at launch.

--- main.py
+++ main.py
@@ -61,47 +61,51 @@ while True:
     cursor.execute("SELECT DocumentID, PaymentTypeID, Amount FROM "+database+".dbo.Payment ORDER BY DocumentID DESC")
     row = cursor.fetchone()
 
-    if StoredID < row[0]:
+    if StoredID == 0:
         StoredID = row[0]
+    
+    else:
+        if StoredID < row[0]:
+            StoredID = row[0]
 
-        if row[1] == os.environ['PaymentID']:
-            
-            while True:
-            
-                    # Convert Amount into string with 2 decimals, as required by the API
-                ConvertedAmount = "{:.2f}".format(row[2])
+            if row[1] == os.environ['PaymentID']:
+                
+                while True:
+                
+                        # Convert Amount into string with 2 decimals, as required by the API
+                    ConvertedAmount = "{:.2f}".format(row[2])
 
-                # Generate TransactionRef
-                TransactionRef = ''.join(random.choices(string.ascii_uppercase + string.digits, k=9))
+                    # Generate TransactionRef
+                    TransactionRef = ''.join(random.choices(string.ascii_uppercase + string.digits, k=9))
 
-                # Create Signature
-                SignatureData = f"0;2;{os.environ['SepayLogin']};{str(os.environ['SID'])};{TransactionRef};{str(row[0])};{ConvertedAmount};"
-                SignatureSign = crypto.sign(pkey, SignatureData, "sha256")
+                    # Create Signature
+                    SignatureData = f"0;2;{os.environ['SepayLogin']};{str(os.environ['SID'])};{TransactionRef};{str(row[0])};{ConvertedAmount};"
+                    SignatureSign = crypto.sign(pkey, SignatureData, "sha256")
 
-                RequestResult = Client.service.StartTransaction(key_index=0, version="2", login=os.environ['SepayLogin'], sid=os.environ['SID'], transactionref=TransactionRef, merchantref=str(row[0]), amount=ConvertedAmount, signature=SignatureSign)
+                    RequestResult = Client.service.StartTransaction(key_index=0, version="2", login=os.environ['SepayLogin'], sid=os.environ['SID'], transactionref=TransactionRef, merchantref=str(row[0]), amount=ConvertedAmount, signature=SignatureSign)
 
-                # Check status code and retry if neccesary 
-                match RequestResult["status"]:
-                    case 0:
-                        break
-                    case 1:
-                        print("Some of the required fields are missing: "+ RequestResult['message'])
-                        break
-                    case 2:
-                        print("Signature failed")
-                        break
-                    case 4:
-                        print("Invalid parameters, retrying...")
-                        continue
-                    case 6:
-                        print("Duplicate request")
-                        break
-                    case 7:
-                        print("Terminal not active or not enabled and/or authorized for transactions through WECR")
-                        break
-                    case 11:
-                        print("Pending request for this terminal.")
-                        break
-                    case 99:
-                        print("Undefined error")
-                        break
+                    # Check status code and retry if neccesary 
+                    match RequestResult["status"]:
+                        case 0:
+                            break
+                        case 1:
+                            print("Some of the required fields are missing: "+ RequestResult['message'])
+                            break
+                        case 2:
+                            print("Signature failed")
+                            break
+                        case 4:
+                            print("Invalid parameters, retrying...")
+                            continue
+                        case 6:
+                            print("Duplicate request")
+                            break
+                        case 7:
+                            print("Terminal not active or not enabled and/or authorized for transactions through WECR")
+                            break
+                        case 11:
+                            print("Pending request for this terminal.")
+                            break
+                        case 99:
+                            print("Undefined error")
+                            break
